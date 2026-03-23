a44fa0dd19460b81ca42c8a26cca8895982d1c88
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-23 13:23:30 -0500

    classnames

--- src/components/SigilRoomHome/Grimiore/Grimoire.tsx
+++ src/components/SigilRoomHome/Grimiore/Grimoire.tsx
@@ -3,28 +3,29 @@ import BackButton from "../../Parts/BackButton"
 import { Link } from 'react-router-dom'
 
 export default function Grimoire({ user }: { user: any }) {
-  
+
   return (
     <div className={'grimoire'}>
       <h1> {user.user}'s Grimoire </h1>
+      
+      <div className={'bookbox'}>
+        <div className={'leftpage'}>
+          <br />
+          <Link to="/map"> Map </Link>
+          <br />
+          <Link to="/scrye-friends"> Scrye Friends </Link>
+          <br />
+          <Link to="/profile" > Profile </Link>
+          <br />
+        </div>
 
-      <div className={'leftpage'}>
+        <div className={'rightpage'}>
+          <Link to="/library"> SigiLibrary </Link>
+        </div>
         <br />
-        <Link to="/map"> Map </Link>
         <br />
-        <Link to="/scrye-friends"> Scrye Friends </Link>
-        <br />
-        <Link to="/profile" > Profile </Link>
         <br />
       </div>
-
-      <div className={'rightpage'}>
-        <Link to="/library"> SigiLibrary </Link>
-      </div>
-      <br />
-      <br />
-      <br />
-
       <BackButton />
     </div>
   )
