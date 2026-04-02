047eccda945bb41aa17f447fd8d64a262cd7357f
HopeyClarkey <thehopeclarke@gmail.com>
2026-04-01 19:53:04 -0500

    still tsing auth

--- server/prisma/prisma.client.ts
+++ server/prisma/prisma.client.ts
@@ -2,20 +2,15 @@ import 'dotenv/config';
 import { PrismaClient } from '../prisma/generated/client.js';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-
-if (!process.env.DATABASE_URL){
-  throw new Error ('No db url in env!')
-}
-const url = new URL(process.env.DATABASE_URL)
-
 const adapter = new PrismaMariaDb({
-  host: url.hostname,
-  user: url.username,
-  password: url.password || undefined,
-  database: url.pathname.slice(1),
-  port: url.port ? parseInt(url.port) : 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   connectionLimit: 5,
 });
+
 const prisma = new PrismaClient({ adapter });
 
-export default  prisma ;
+export default prisma;
